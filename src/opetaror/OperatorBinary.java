753fa45bf52bb51890c53302f06554580e15fc7b
muhamadnoval <mnoval292@gmail.com>
2020-04-12 08:38:50 +0700

    Operasi Binary Assigment

---
+++ src/opetaror/OperatorBinary.java
@@ -0,0 +1,12 @@
+package opetaror;
+
+public class OperatorBinary {
+    //operator (=) sebagai assigment / penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Muhamad Noval";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
