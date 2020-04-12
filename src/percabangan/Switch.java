a285de43aa4464234a20f262eeb0e441356ebaee
muhamadnoval <mnoval292@gmail.com>
2020-04-12 10:01:47 +0700

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,23 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanjalan=3;
+        switch (jalanjalan){
+            case 1:
+                System.out.println("iwak lapas");
+                break;
+            case 2:
+                System.out.println("Manusia hamuk");
+                break;
+            case 3:
+                System.out.println("hayam lapas");
+                break;
+            case 4:
+                System.out.println("dubbill kiill");
+                break;
+            case 5:
+                System.out.println("Makhluk ajaib");
+        }
+    }
+}
